09549c54f78bae9ab2d12e84184c5daf72b51201
blakolukas <lucasblacovicz@gmail.com>
2025-06-14 16:28:28 -0300

    primeiro commit

---
+++ src/main/java/com/imposto/TaxesService/TaxesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.imposto.TaxesService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxesServiceApplication.class, args);
+	}
+
+}
